b5dcadc97fd954a8ab84a94fdd37744f0be51397
唐司军 <tangsijun@uniontech3d.com>
2020-03-22 22:31:20 +0800

    chore: update

--- packages/rtw-host-app/src/pages/Article/containers/ArticleList/index.tsx
+++ packages/rtw-host-app/src/pages/Article/containers/ArticleList/index.tsx
@@ -1,4 +1,5 @@
-import { BackTop, Divider, Pagination } from 'antd';
+import { Avatar, BackTop, Card, Divider, Icon, Pagination } from 'antd';
+import _ from 'lodash';
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { RouteComponentProps, withRouter } from 'react-router';
@@ -6,6 +7,7 @@ import { RouteComponentProps, withRouter } from 'react-router';
 import { IState } from '@/ducks';
 import { articleActions } from '@/pages/article/ducks/blog';
 import { tagActions } from '@/pages/article/ducks/tag';
+import { userActions } from '@/pages/userCenter/ducks/profile';
 import * as S from '@/schema';
 
 import { ArticleCard } from '../../components/ArticleCard';
@@ -13,11 +15,17 @@ import { ArticleTag } from '../ArticleTags';
 
 import * as styles from './index.less';
 
+const IconFont = Icon.createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_1261840_moszi2ku3d.js',
+});
+
 export interface ArticleListProps extends RouteComponentProps {
   tagList?: string[];
   articleCount: number;
+  profile: S.UserProfile;
   articleList: S.Article[];
 
+  loadProfile: (id: number) => void;
   loadTagList: () => void;
   loadArticleByTags: (
     pageNum: number,
@@ -30,6 +38,7 @@ export interface ArticleListState {
   checkedTags: string[];
   pageNum: number;
   pageSize: number;
+  isShowProfile: boolean;
 }
 
 export class ArticleListComp extends React.Component<
@@ -40,19 +49,34 @@ export class ArticleListComp extends React.Component<
     super(props);
 
     this.state = {
-      checkedTags: [],
       pageNum: 1,
       pageSize: 10,
+      checkedTags: [],
+      isShowProfile: true,
     };
   }
 
   componentDidMount() {
     this.onRefresh();
+    this.screenChange();
   }
 
   onRefresh = () => {
     this.props.loadTagList();
+    this.props.loadProfile(1);
     this.props.loadArticleByTags(0, 10);
+  };
+
+  screenChange = () => {
+    window.addEventListener('resize', this.handleChange);
+  };
+
+  handleChange = () => {
+    const width = window.innerWidth;
+
+    width < 850
+      ? this.setState({ isShowProfile: false })
+      : this.setState({ isShowProfile: true });
   };
 
   setCheckedTags = (checkedTags: string[]) => {
@@ -67,13 +91,68 @@ export class ArticleListComp extends React.Component<
     this.props.loadArticleByTags(pageNum - 1, pageSize, checkedTags);
   };
 
+  renderProfile = () => {
+    const { profile } = this.props;
+
+    return (
+      <div className={styles.cardList}>
+        <Card
+          hoverable={true}
+          className={styles.profile}
+          loading={_.isNil(profile)}
+        >
+          <div className={styles.header}>
+            <div className={styles.title}>
+              <p>{profile.username}</p>
+              <span>{_.truncate(profile.signature, { length: 15 })}</span>
+            </div>
+            <div>
+              <Avatar src={profile.avatarUrl} />
+            </div>
+          </div>
+          <Divider style={{ margin: '12px 0' }} />
+          <div className={styles.profileContent}>
+            <div>
+              <Icon type="environment" className={styles.icon} />
+              <span>{_.truncate(profile.address, { length: 25 })}</span>
+            </div>
+            <div>
+              <Icon type="book" className={styles.icon} />
+              <span>{_.truncate(profile.university, { length: 25 })}</span>
+            </div>
+            <div>
+              <Icon type="mail" className={styles.icon} />
+              <span>{_.truncate(profile.email, { length: 25 })}</span>
+            </div>
+            <div>
+              <Icon type="github" className={styles.icon} />
+              <span>{_.truncate(profile.github, { length: 25 })}</span>
+            </div>
+          </div>
+        </Card>
+        <Card hoverable={true} className={styles.articleInfo}>
+          <div className={styles.header}>
+            <span>文章归档</span>
+            <IconFont type="icon-placeFile" />
+          </div>
+        </Card>
+      </div>
+    );
+  };
+
   render() {
     const { tagList, articleList, articleCount } = this.props;
 
     return (
-      <div className={styles.f}>
-        <BackTop visibilityHeight={100} />
-        <div className={styles.s}>
+      <div className={styles.container}>
+        <BackTop visibilityHeight={200} style={{ right: 40 }}>
+          <IconFont
+            type="icon-backTop"
+            style={{ fontSize: 40, color: '#6874e2' }}
+          />
+        </BackTop>
+        {this.state.isShowProfile && this.renderProfile()}
+        <div className={styles.articleList}>
           <div className={styles.selectTag}>
             <span>所属标签：</span>
             <ArticleTag tagList={tagList} checkedTags={this.setCheckedTags} />
@@ -94,11 +173,13 @@ export class ArticleListComp extends React.Component<
 export const ArticleList = connect(
   (state: IState) => ({
     tagList: state.blog.tag.tagList,
+    profile: state.user.profile.profile,
     articleList: state.blog.article.articleList,
     articleCount: state.blog.article.articleCount,
   }),
   {
     loadTagList: tagActions.loadTagList,
+    loadProfile: userActions.loadProfile,
     loadArticleByTags: articleActions.loadArticleByTags,
   },
 )(withRouter(ArticleListComp));
