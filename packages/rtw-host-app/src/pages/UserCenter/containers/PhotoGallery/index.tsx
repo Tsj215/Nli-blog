ff55a705f89dc3451a30eee579c2ad4c8eaef58a
唐司军 <tangsijun@uniontech3d.com>
2020-04-19 16:41:00 +0800

    chore: update

--- packages/rtw-host-app/src/pages/UserCenter/containers/PhotoGallery/index.tsx
+++ packages/rtw-host-app/src/pages/UserCenter/containers/PhotoGallery/index.tsx
@@ -2,6 +2,7 @@ import { Icon, Upload, message } from 'antd';
 import { RcFile, UploadChangeParam } from 'antd/lib/upload/interface';
 import _ from 'lodash';
 import * as React from 'react';
+import Carousel, { Modal, ModalGateway } from 'react-images';
 import Gallery from 'react-photo-gallery';
 import { connect } from 'react-redux';
 import { RouteComponentProps, withRouter } from 'react-router';
@@ -11,6 +12,10 @@ import { IState } from '@/ducks';
 import * as S from '@/schema';
 
 import * as styles from './index.less';
+
+const IconFont = Icon.createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_1261840_61nija66ljl.js',
+});
 
 export interface PhotoGalleryProps extends RouteComponentProps {
   photos: S.Photo[];
@@ -22,7 +27,11 @@ export interface PhotoGalleryState {
   key?: string;
   uploadToken?: string;
 
+  selectedId: number[];
+  currentPhoto: number;
   isUploading: boolean;
+  isShowLightBox: boolean;
+  isEditPhoto: boolean;
 }
 
 export class PhotoGalleryComp extends React.Component<
@@ -33,7 +42,11 @@ export class PhotoGalleryComp extends React.Component<
     super(props);
 
     this.state = {
+      selectedId: [],
+      currentPhoto: 0,
+      isEditPhoto: false,
       isUploading: false,
+      isShowLightBox: false,
     };
   }
 
@@ -64,6 +77,64 @@ export class PhotoGalleryComp extends React.Component<
     }
   };
 
+  handleLightBox = (__: any, { index }: { index: number }) => {
+    console.log('index', index);
+    this.setState({ isShowLightBox: true, currentPhoto: index });
+  };
+
+  handlePhotoGallery = (photo: any) => {
+    const { selectedId } = this.state;
+
+    if (_.includes(selectedId, photo.id)) {
+      this.setState({ selectedId: _.pull(selectedId, photo.id) }, () =>
+        console.log(this.state.selectedId),
+      );
+    } else {
+      selectedId.push(photo.id);
+      this.setState({ selectedId }, () => console.log(this.state.selectedId));
+    }
+  };
+
+  renderPhoto = ({ index, photo }: any) => {
+    const { selectedId } = this.state;
+    const imgStyle = {
+      transition:
+        'transform .135s cubic-bezier(0.0,0.0,0.2,1),opacity linear .15s',
+    };
+    const selectedImgStyle = {
+      transform: 'translateZ(0px) scale3d(0.9, 0.9, 1)',
+      transition:
+        'transform .135s cubic-bezier(0.0,0.0,0.2,1),opacity linear .15s',
+    };
+
+    const isSelected = _.includes(selectedId, photo.id);
+
+    const iconStyle: any = { display: 'none' };
+    const selectedIconStyle = {
+      top: '0',
+      left: '0',
+      zIndex: '1',
+      fontSize: 24,
+      position: 'absolute',
+    };
+
+    return (
+      <div key={index} className={styles.renderPhoto}>
+        <IconFont
+          style={isSelected ? selectedIconStyle : iconStyle}
+          type="icon-check4"
+        />
+        <img
+          style={_.includes(selectedId, photo.id) ? selectedImgStyle : imgStyle}
+          src={photo.src}
+          width={photo.width}
+          height={photo.height}
+          onClick={() => this.handlePhotoGallery(photo)}
+        />
+      </div>
+    );
+  };
+
   render() {
     const { photos } = this.props;
     const { uploadToken, key } = this.state;
@@ -71,12 +142,15 @@ export class PhotoGalleryComp extends React.Component<
     return (
       <div className={styles.container}>
         <Gallery
-          margin={2}
+          margin={6}
           photos={(photos || []).map(p => ({
+            id: p.id,
             src: p.url,
-            width: p.width,
-            height: p.height,
+            width: 1,
+            height: 1,
+            sizes: ['(min-width: 480px) 50vw,(min-width: 1024px) 33.3vw,100vw'],
           }))}
+          renderImage={this.renderPhoto}
         />
         <Upload
           listType="picture-card"
@@ -92,6 +166,20 @@ export class PhotoGalleryComp extends React.Component<
             <Icon type="plus" />
           )}
         </Upload>
+        <ModalGateway>
+          {this.state.isShowLightBox && (
+            <Modal
+              onClose={() =>
+                this.setState({ currentPhoto: 0, isShowLightBox: false })
+              }
+            >
+              <Carousel
+                currentIndex={this.state.currentPhoto}
+                views={(photos || []).map(p => ({ source: p.url }))}
+              />
+            </Modal>
+          )}
+        </ModalGateway>
       </div>
     );
   }
