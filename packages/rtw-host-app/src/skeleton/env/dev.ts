1b1e0893affeb8592c7019d869d7897921de1590
唐司军 <tangsijun@uniontech3d.com>
2020-04-20 14:50:58 +0800

    chore: update

--- packages/rtw-host-app/src/skeleton/env/dev.ts
+++ packages/rtw-host-app/src/skeleton/env/dev.ts
@@ -1,7 +1,7 @@
 import MobileDetect from 'mobile-detect';
 
-export const HOST = 'http://localhost:3030';
-// export const HOST = 'http://106.52.97.183:3030';
+// export const HOST = 'http://localhost:3030';
+export const HOST = 'http://106.52.97.183:3030';
 export const WITH_AUTH = true;
 
 const md = new MobileDetect(window.navigator.userAgent);
